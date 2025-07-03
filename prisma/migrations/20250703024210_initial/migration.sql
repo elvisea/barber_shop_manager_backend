47d8f73c32e87e70d34ce4a40c5df88e1dd6786d
Elvis <eleram@protonmail.com>
2025-07-02 23:44:21 -0300

    refactor(prisma): mantém apenas tabela User com UUID e enum Role; seed ajustado para novos usuários

---
+++ prisma/migrations/20250703024210_initial/migration.sql
@@ -0,0 +1,22 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('RECEPTIONIST', 'HAIRDRESSER', 'BARBER', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "Role" NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE INDEX "users_email_idx" ON "users"("email");
