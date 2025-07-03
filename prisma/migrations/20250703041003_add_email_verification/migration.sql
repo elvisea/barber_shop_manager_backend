a54aad42c73f84aa76ec8fc9cac97dac0bf77be9
Elvis <eleram@protonmail.com>
2025-07-03 01:10:08 -0300

    feat(prisma): add email verification feature

---
+++ prisma/migrations/20250703041003_add_email_verification/migration.sql
@@ -0,0 +1,23 @@
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN     "email_verified" BOOLEAN NOT NULL DEFAULT false;
+
+-- CreateTable
+CREATE TABLE "email_verifications" (
+    "id" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "expires_at" TIMESTAMP(3) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "user_id" TEXT NOT NULL,
+
+    CONSTRAINT "email_verifications_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "email_verifications_token_key" ON "email_verifications"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "email_verifications_user_id_key" ON "email_verifications"("user_id");
+
+-- AddForeignKey
+ALTER TABLE "email_verifications" ADD CONSTRAINT "email_verifications_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
