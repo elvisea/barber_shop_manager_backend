634280e77543dfb7893293b898a1f4ef9107292c
Elvis <eleram@protonmail.com>
2025-07-02 23:17:41 -0300

    feat(email): adiciona EmailModule e EmailService ao projeto

---
+++ src/email/email.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+
+import { MailerModule } from '@nestjs-modules/mailer';
+import { ConfigService } from '@nestjs/config';
+
+import { EmailService } from './email.service';
+
+@Module({
+  imports: [
+    MailerModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        transport: {
+          host: configService.get('MAIL_HOST'),
+          port: configService.get('MAIL_PORT'),
+          secure: configService.get('MAIL_SECURE') === 'true',
+          auth: {
+            user: configService.get('MAIL_USER'),
+            pass: configService.get('MAIL_PASS'),
+          },
+        },
+      }),
+    }),
+  ],
+  providers: [EmailService],
+  exports: [EmailService],
+})
+export class EmailModule { }
