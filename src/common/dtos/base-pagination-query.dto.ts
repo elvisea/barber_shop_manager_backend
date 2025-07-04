afbf4629acb6997e566330a4a2eda6404ed6aec5
Elvis <eleram@protonmail.com>
2025-07-04 16:39:39 -0300

    refactor(dto): added @Type(() => Number) in BasePaginationQueryDTO

--- src/common/dtos/base-pagination-query.dto.ts
+++ src/common/dtos/base-pagination-query.dto.ts
@@ -1,15 +1,18 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
 import { IsInt, IsOptional, Max, Min } from 'class-validator';
 
 export class BasePaginationQueryDTO {
   @ApiPropertyOptional({ example: 1 })
   @IsOptional()
+  @Type(() => Number)
   @IsInt()
   @Min(1)
   page?: number = 1;
 
   @ApiPropertyOptional({ example: 10, maximum: 50 })
   @IsOptional()
+  @Type(() => Number)
   @IsInt()
   @Min(1)
   @Max(50)
