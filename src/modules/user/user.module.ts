b7d7cfb8c0a07fef2d551c02128baa7f32884f0c
Elvis <eleram@protonmail.com>
2025-08-04 22:31:17 -0300

    refactor(user): remove create-user-member functionality
    - Remove create-user-member.controller.ts
    - Remove create-user-member.service.ts
    - Remove create-user-member-request.dto.ts
    - Update user.module.ts to remove references
    - Clean up unused code and dependencies

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,17 +1,15 @@
 import { Module } from '@nestjs/common';
 
-import { CreateUserMemberController } from './controllers/create-user-member.controller';
 import { CreateUserController } from './controllers/create-user.controller';
 import { UserRepository } from './repositories/user.repository';
-import { CreateUserMemberService } from './services/create-user-member.service';
 import { CreateUserService } from './services/create-user.service';
 
 import { UserEmailVerificationModule } from '@/modules/user-email-verification/user-email-verification.module';
 
 @Module({
   imports: [UserEmailVerificationModule],
-  controllers: [CreateUserController, CreateUserMemberController],
-  providers: [CreateUserService, CreateUserMemberService, UserRepository],
+  controllers: [CreateUserController],
+  providers: [CreateUserService, UserRepository],
   exports: [UserRepository, CreateUserService],
 })
 export class UserModule {}
